d4744cba89f98061e5ab12be6ec676bde47f1939
laktam abdellah <laktamlaktamabdellah@gmail.com>
2023-12-31 21:31:01 +0100

    update Type.java to store relationships

--- Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction2/Type.java
+++ Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction2/Type.java
@@ -1,9 +1,26 @@
 package org.mql.java.extraction2;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Type {
 	protected java.lang.Class<?> c;
+	protected List<Relationship> relationships;
+	
+	public Type(java.lang.Class<?> c) {
+		this.relationships = new Vector<Relationship>();
+		this.c = c;
+	}
 	
 	public java.lang.Class<?> getObjectClass() {
 		return c;
+	}
+	
+	public void addRelationship(Relationship r) {
+		relationships.add(r);
+	}
+	
+	public  List<Relationship> getRelationships(){
+		return relationships;
 	}
 }
