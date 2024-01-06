818c22df2226bf90e5d49e626a5a22139e3e0b8f
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-01-06 15:35:55 +0100

    update PackageType

--- Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction/PackageType.java
+++ Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction/PackageType.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 
 public class PackageType {
 	private String name;
+	private List<PackageType> packages;
 	private List<ClassType> classes;
 	private List<InterfaceType> interfaces;
 	private List<AnnotationType> annotations;
@@ -12,12 +13,66 @@ public class PackageType {
 	
 	PackageType(String name){
 		this.name = name;
+		this.packages = new Vector<PackageType>();
 		this.classes = new Vector<ClassType>();
 		this.interfaces = new Vector<InterfaceType>();
 		this.enumerations = new Vector<EnumerationType>();
 		this.annotations = new Vector<AnnotationType>();
 	}
 	
+	@Override
+	public String toString() {
+//		String s = name + "\n";
+//		for (PackageType p : packages) {
+//			s += "- " + p + "\n";
+//		}
+		return printTree("\t");
+	}
+	
+	public String printTree(String indentation) {
+		String s = indentation + name  + "\n";
+		
+		if(!classes.isEmpty()) {
+			s+= indentation + " ** classes \n";
+			for (ClassType c : classes) {
+				s+= indentation + " -- "  + c.getName() + "\n";
+			}	
+		}
+		
+		
+		if(!interfaces.isEmpty()) {
+			s+= indentation + " ** interfaces \n";		
+			for (InterfaceType i : interfaces) {
+				s+= indentation + " -- "  + i.getName() + "\n";
+			}	
+		}
+		
+		if(!packages.isEmpty()) {
+			indentation += "\t";
+			for (PackageType p : packages) {
+				s += p.printTree(indentation);
+			}
+		}
+		
+		return s;
+	}
+	
+	public void addPackages(List<PackageType> packages) {
+		this.packages.addAll(packages);
+	}
+	
+	public List<PackageType> getPackages() {
+		return packages;
+	}
+
+	public void setPackages(List<PackageType> packages) {
+		this.packages = packages;
+	}
+
+	public void addPackage(PackageType p) {
+		packages.add(p);
+	}
+
 	public void addEnumeration(EnumerationType e) {
 		enumerations.add(e);
 	}
@@ -30,7 +85,7 @@ public class PackageType {
 		annotations.add(a);
 	}
 	
-	public List<AnnotationType> getAnnotation(){
+	public List<AnnotationType> getAnnotations(){
 		return annotations;
 	}
 	
