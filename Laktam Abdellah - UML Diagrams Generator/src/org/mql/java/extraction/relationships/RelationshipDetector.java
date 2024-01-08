ffb0b6af893637377d74860d65c46c4464b3f96f
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-01-09 00:20:18 +0100

    before trying to get Collection parameters

--- Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction/relationships/RelationshipDetector.java
+++ Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/extraction/relationships/RelationshipDetector.java
@@ -3,7 +3,11 @@ package org.mql.java.extraction.relationships;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.TypeVariable;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import org.mql.java.extraction.ClassType;
 import org.mql.java.extraction.FieldType;
@@ -29,8 +33,20 @@ public class RelationshipDetector {
 			for (FieldType f : fields) {
 
 				Class<?> fieldClass = f.getType();
-				if (!isSimple(fieldClass)) {// !fieldClass.isPrimitive() && !fieldClass.equals(String.class)
-					type.addRelationship(new Relationship("agregation", type, new ClassType(fieldClass)));
+				if (!isSimple(fieldClass)) {
+					// test if it's a collection
+					TypeVariable<?>[] typeVariables = getCollectionTypeVariables(fieldClass);
+					if (typeVariables != null) {
+						for (TypeVariable<?> typeVariable : typeVariables) {
+							System.out.println(typeVariable.getTypeName());
+							type.addRelationship(
+									new Relationship("agregation", type, new ClassType(typeVariable.getClass())));
+
+						}
+					} else {
+						type.addRelationship(new Relationship("agregation", type, new ClassType(fieldClass)));
+					}
+
 				}
 			}
 
@@ -55,6 +71,25 @@ public class RelationshipDetector {
 			}
 		}
 
+	}
+
+	private static TypeVariable<?>[] getCollectionTypeVariables(Class<?> c) {
+		// implements iterable or Map
+		List<Class<?>> superInterfaces = new Vector<Class<?>>();
+		getAllSuperInterfaces(c, superInterfaces);
+		if (superInterfaces.contains(Iterable.class) || superInterfaces.contains(Map.class)) {
+//			System.out.println(c.getTypeParameters());
+			return c.getTypeParameters();
+		}
+		return null;
+	}
+	
+	private static void getAllSuperInterfaces(Class<?> c, List<Class<?>> interfaces){
+		Class<?> list[] = c.getInterfaces();
+		interfaces.addAll(List.of(list));
+		for (Class<?> i : list) {
+			getAllSuperInterfaces(i , interfaces);
+		}
 	}
 
 	private static boolean isSimple(java.lang.Class<?> c) {
