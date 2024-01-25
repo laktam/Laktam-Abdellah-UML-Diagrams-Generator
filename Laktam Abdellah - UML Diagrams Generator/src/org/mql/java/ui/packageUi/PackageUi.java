8c18dc70fe0c79abf5dad5b7c3092532f6db1016
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-01-25 03:14:06 +0100

    improve package diagram drawing

--- Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/ui/packageUi/PackageUi.java
+++ Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/ui/packageUi/PackageUi.java
@@ -26,7 +26,9 @@ public class PackageUi extends JPanel {
 	private JPanel body;
 	private List<PackageUi> subPackagesUi;
 	private int bottomPadding = 15;
-	
+	private int insidePackageBottomPadding = 30;
+	private int insidePackageRightPadding = 15;
+
 	public PackageUi(PackageType pckg, boolean root) {
 		this.subPackagesUi = new Vector<PackageUi>();
 		this.root = root;// we draw the fqName only for the root
@@ -65,20 +67,39 @@ public class PackageUi extends JPanel {
 //		}
 //	}
 	public void fillBody() {
+		int maxW = calculateMaxSubPackageWidth();
+		int maxH = calculateMaxSubPackageHeight();
+		
+
 		// add counter to add two in each line
 		Dimension d = new Dimension(padding, header.getHeight() + typesPanel.getHeight() + padding * 2);
-		int counter = 0;
+		int counter = 1;
 		for (PackageType subP : subPackages) {
 			PackageUi subPUi = new PackageUi(subP, false);
 			Dimension subD = subPUi.calculateSize();
-//			body.add(subPUi);
-			add(subPUi);
-			subPackagesUi.add(subPUi);
-			subPUi.setLocation(d.width, d.height);
-			d.width += subD.width + subD.width / 2;
+
+			if (counter % 2 == 0) {
+				// draw and go to start of the line => increase d.height by the tallest of the
+				// last two packages
+				//
+				add(subPUi);
+				subPackagesUi.add(subPUi);
+				subPUi.setLocation(d.width, d.height);
+				d.width = padding;//go to next line X
+				d.height += maxH + insidePackageBottomPadding;//go to next line Y
+//				d.height += Math.max(subPackagesUi.get(subPackagesUi.size() - 2).getHeight(),
+//						subPackagesUi.get(subPackagesUi.size() - 1).getHeight()) + insidePackageBottomPadding;
+				counter++;
+			} else {
+				add(subPUi);
+				subPackagesUi.add(subPUi);
+				subPUi.setLocation(d.width, d.height);
+				d.width += maxW + insidePackageRightPadding;// go to second column in line
+				counter++;
+			}
+
 //			d.height += subD.height;//we will just place them horizontally
 		}
-
 
 	}
 
@@ -88,23 +109,52 @@ public class PackageUi extends JPanel {
 //	}
 
 	public Dimension calculateSize() {
+		// h : the height of the tallest package * lines + types + header
+		// w : the w of the largerst package * 2 + paddings ?
+		int maxH = calculateMaxSubPackageHeight();
+		int maxW = calculateMaxSubPackageWidth();
+
 		if (subPackages.isEmpty()) {
-//			return new Dimension(500, 500);
-			return new Dimension(typesPanel.getWidth() + padding * 2,
+			int w  = Math.max(typesPanel.getWidth(), header.getWidth());//sometimes the fqname is wider than content
+			return new Dimension(w + padding * 2,
 					header.getHeight() + typesPanel.getHeight() + padding * 2);
 		} else {
 			Dimension d = new Dimension();
-			for (PackageType subP : subPackages) {
-				PackageUi subPUi = new PackageUi(subP, false);
-				Dimension subD = subPUi.calculateSize();
-				d.width += subD.width;
-				d.height += subD.height;
-			}
-			d.width += typesPanel.getWidth() + padding * 2;// compare typespanel width with the sum of subPckgs with and
-															// set w to max
-			d.height += header.getHeight() + typesPanel.getHeight() + bottomPadding;
+			d.width = maxW * 2 + insidePackageRightPadding * 2;// + 50;
+			d.height = (maxH + insidePackageBottomPadding) * ((subPackages.size() + 1) / 2) + bottomPadding;
+			
+//			d.width += typesPanel.getWidth();
+			d.height += typesPanel.getHeight();
+			d.height += header.getHeight();
+//			for (PackageType subP : subPackages) {
+//				PackageUi subPUi = new PackageUi(subP, false);
+//				Dimension subD = subPUi.calculateSize();
+//				d.width += subD.width;
+//				d.height += subD.height;
+//			}
+//			d.width += typesPanel.getWidth() + padding * 2;// compare typespanel width with the sum of subPckgs with and
+//															// set w to max
+//			d.height += header.getHeight() + typesPanel.getHeight() + bottomPadding;
 			return d;
 		}
+
+//		if (subPackages.isEmpty()) {
+////			return new Dimension(500, 500);
+//			return new Dimension(typesPanel.getWidth() + padding * 2,
+//					header.getHeight() + typesPanel.getHeight() + padding * 2);
+//		} else {
+//			Dimension d = new Dimension();
+//			for (PackageType subP : subPackages) {
+//				PackageUi subPUi = new PackageUi(subP, false);
+//				Dimension subD = subPUi.calculateSize();
+//				d.width += subD.width;
+//				d.height += subD.height;
+//			}
+//			d.width += typesPanel.getWidth() + padding * 2;// compare typespanel width with the sum of subPckgs with and
+//															// set w to max
+//			d.height += header.getHeight() + typesPanel.getHeight() + bottomPadding;
+//			return d;
+//		}
 	}
 
 	@Override
@@ -116,6 +166,36 @@ public class PackageUi extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawRect(0, header.getHeight(), getSize().width - 2, getSize().height - header.getHeight() - 2);
+	}
+
+	public int calculateMaxSubPackageWidth() {
+		int maxW = 0;
+		List<PackageUi> subPckgs = new Vector<>();
+		for (PackageType p : subPackages) {
+			subPckgs.add(new PackageUi(p, false));
+		}
+
+		for (PackageUi p : subPckgs) {
+			if (p.getWidth() > maxW) {
+				maxW = p.getWidth();
+			}
+		}
+		return maxW;
+	}
+
+	public int calculateMaxSubPackageHeight() {
+		int maxH = 0;
+		List<PackageUi> subPckgs = new Vector<>();
+		for (PackageType p : subPackages) {
+			subPckgs.add(new PackageUi(p, false));
+		}
+
+		for (PackageUi p : subPckgs) {
+			if (p.getHeight() > maxH) {
+				maxH = p.getHeight();
+			}
+		}
+		return maxH;
 	}
 
 }
