04c0530e61c4abca48a88ae07dbf421c4f54dec6
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-01-25 18:19:56 +0100

    read from xml and draw and print from it

--- Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/xml/Parser.java
+++ Laktam Abdellah - UML Diagrams Generator/src/org/mql/java/xml/Parser.java
@@ -3,12 +3,13 @@ package org.mql.java.xml;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -22,10 +23,129 @@ import org.mql.java.extraction.*;
 import org.mql.java.extraction.relationships.Relationship;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class Parser {
 	private static Document doc;
 	private static Element classDiagrams;
+
+	public static Project read(String source) {
+		try {
+			doc = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().parse(source);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		classDiagrams = doc.getDocumentElement();
+		Project project = new Project(classDiagrams.getAttribute("name"));
+		// get packages
+		List<PackageType> packages = new Vector<PackageType>();
+		NodeList packagesElements = classDiagrams.getElementsByTagName("classDiagram");
+		for (int i = 0; i < packagesElements.getLength(); i++) {
+			Element pckgE = (Element) packagesElements.item(i);
+			PackageType pckgType = new PackageType(pckgE.getAttribute("name"));
+			// classes
+			List<ClassType> classes = new Vector<ClassType>();
+			NodeList classesElements = pckgE.getElementsByTagName("class");
+			for (int j = 0; j < classesElements.getLength(); j++) {
+				Element classE = (Element) classesElements.item(j);
+				String classFQName = pckgE.getAttribute("name") + "." + classE.getAttribute("name");
+				ClassType c = new ClassType(classFQName, classE.getAttribute("name"));
+				// fields
+				List<FieldType> fieldsTypes = new Vector<FieldType>();
+				NodeList fieldsElements = classE.getElementsByTagName("field");
+				for (int f = 0; f < fieldsElements.getLength(); f++) {
+					Element fieldE = (Element) fieldsElements.item(f);
+					FieldType fType = new FieldType(fieldE.getAttribute("name"), fieldE.getAttribute("modifiers"),
+							fieldE.getAttribute("type"));
+					fieldsTypes.add(fType);
+				}
+				c.setFields(fieldsTypes);
+				// methodes
+				List<MethodType> methods = new Vector<MethodType>();
+				NodeList methodsElements = classE.getElementsByTagName("method");
+				for (int m = 0; m < methodsElements.getLength(); m++) {
+					Element methodE = (Element) methodsElements.item(m);
+					MethodType mType = new MethodType(methodE.getAttribute("name"), methodE.getAttribute("modifiers"),
+							methodE.getAttribute("returnType"));
+					// parameters
+					List<ParameterType> params = new Vector<ParameterType>();
+					NodeList paramsElements = methodE.getElementsByTagName("parameter");
+					for (int p = 0; p < paramsElements.getLength(); p++) {
+						Element paramE = (Element) paramsElements.item(p);
+						ParameterType param = new ParameterType(paramE.getAttribute("name"),
+								paramE.getAttribute("type"));
+						params.add(param);
+					}
+					mType.setParameters(params);
+					methods.add(mType);
+				}
+				c.setMethods(methods);
+				// relationships
+				List<Relationship> relationships = new Vector<Relationship>();
+				Element relationsRoot = (Element) classE.getElementsByTagName("relationships").item(0);
+				NodeList relationsElements = relationsRoot.getElementsByTagName("relationship");
+				for (int r = 0; r < relationsElements.getLength(); r++) {
+					Element relationE = (Element) relationsElements.item(r);
+					Relationship relationship = new Relationship(relationE.getAttribute("type"),
+							relationE.getAttribute("from"), relationE.getAttribute("to"));
+					c.addRelationship(relationship);
+				}
+				classes.add(c);
+
+			}
+			// interfaces
+			List<InterfaceType> interfaces = new Vector<InterfaceType>();
+			NodeList intefacesElements = pckgE.getElementsByTagName("interface");
+			for (int in = 0; in < intefacesElements.getLength(); in++) {
+				Element interfaceE = (Element) intefacesElements.item(in);
+				InterfaceType interfaceType = readInterface(interfaceE, pckgE.getAttribute("name"));
+				interfaces.add(interfaceType);
+			}
+
+			pckgType.setClasses(classes);
+			pckgType.setInterfaces(interfaces);
+			packages.add(pckgType);
+		}
+		project.setPackages(packages);
+
+		return project;
+	}
+
+	private static InterfaceType readInterface(Element interfaceE, String pckgName) {
+		String fqName = pckgName + "." + interfaceE.getAttribute("name");
+		InterfaceType interfaceType = new InterfaceType(interfaceE.getAttribute("name"), fqName);
+		//methods
+		List<MethodType> methods = new Vector<MethodType>();
+		NodeList methodsElements = interfaceE.getElementsByTagName("method");
+		for (int m = 0; m < methodsElements.getLength(); m++) {
+			Element methodE = (Element) methodsElements.item(m);
+			MethodType mType = new MethodType(methodE.getAttribute("name"), methodE.getAttribute("modifiers"),
+					methodE.getAttribute("returnType"));
+			// parameters
+			List<ParameterType> params = new Vector<ParameterType>();
+			NodeList paramsElements = methodE.getElementsByTagName("parameter");
+			for (int p = 0; p < paramsElements.getLength(); p++) {
+				Element paramE = (Element) paramsElements.item(p);
+				ParameterType param = new ParameterType(paramE.getAttribute("name"), paramE.getAttribute("type"));
+				params.add(param);
+			}
+			mType.setParameters(params);
+			methods.add(mType);
+		}
+		interfaceType.setMethods(methods);
+		// relationships
+		List<Relationship> relationships = new Vector<Relationship>();
+		Element relationsRoot = (Element) interfaceE.getElementsByTagName("relationships").item(0);
+		NodeList relationsElements = relationsRoot.getElementsByTagName("relationship");
+		for (int r = 0; r < relationsElements.getLength(); r++) {
+			Element relationE = (Element) relationsElements.item(r);
+			Relationship relationship = new Relationship(relationE.getAttribute("type"),
+					relationE.getAttribute("from"), relationE.getAttribute("to"));
+			interfaceType.addRelationship(relationship);
+		}
+		return interfaceType;
+		
+	}
 
 	public static void write(Project project, String output) {
 
@@ -33,6 +153,7 @@ public class Parser {
 			doc = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().newDocument();
 			// add a schema
 			classDiagrams = doc.createElement("classDiagrams");
+			classDiagrams.setAttribute("name", project.getName());
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		}
@@ -56,6 +177,13 @@ public class Parser {
 					Element classElement = classToElement(c);
 					classDiagram.appendChild(classElement);
 				}
+				// interfaces
+				List<InterfaceType> interfaces = p.getInterfaces();
+				for (InterfaceType i : interfaces) {
+					Element iElement = interfaceToElement(i);
+					classDiagram.appendChild(iElement);
+				}
+
 				classDiagrams.appendChild(classDiagram);
 			}
 			// parse subpackages
@@ -112,20 +240,70 @@ public class Parser {
 			}
 			classElement.appendChild(methodElement);
 		}
+		// add relationships to each element
+		Set<Relationship> relationships = c.getRelationshipsSet();
+		Element relationshipsElement = parseRelationships(relationships);
+		classElement.appendChild(relationshipsElement);
 		return classElement;
 	}
 
-	private static void parseRelationships(Set<Relationship> relationships) {
+	// intefaces
+	private static Element interfaceToElement(InterfaceType i) {
+		Element interfaceElement = doc.createElement("interface");
+		interfaceElement.setAttribute("name", i.getSimpleName());
+
+		List<MethodType> methods = i.getMethods();
+		for (MethodType method : methods) {
+			Element methodElement = doc.createElement("method");
+			methodElement.setAttribute("modifiers", method.getModifiers());
+			methodElement.setAttribute("name", method.getName());
+			Type rT = method.getReturnType();
+			if (rT instanceof Class<?>) {
+				methodElement.setAttribute("returnType", ((Class<?>) method.getReturnType()).getSimpleName());
+			} else if (rT instanceof ParameterizedType) {
+				String nameWithParameters = FieldType.createSimpleTypeName((ParameterizedType) rT, "(", ")");
+				methodElement.setAttribute("returnType", nameWithParameters);
+			}
+
+			List<ParameterType> parameters = method.getParameters();
+			for (ParameterType param : parameters) {
+				Element paramElement = doc.createElement("parameter");
+				paramElement.setAttribute("name", param.getName());
+				paramElement.setAttribute("type", param.getTypeSimpleName());
+				methodElement.appendChild(paramElement);
+			}
+			interfaceElement.appendChild(methodElement);
+		}
+		// add relationships to each element
+		Set<Relationship> relationships = i.getRelationshipsSet();
+		Element relationshipsElement = parseRelationships(relationships);
+		interfaceElement.appendChild(relationshipsElement);
+		return interfaceElement;
+	}
+
+	private static Element parseRelationships(Set<Relationship> relationships) {
 		Element relationshipsE = doc.createElement("relationships");
 		for (Relationship r : relationships) {
 			Element rE = doc.createElement("relationship");
 			rE.setAttribute("type", r.getType());
-			rE.setAttribute("from", r.getFrom().getSimpleName());
-			rE.setAttribute("to", r.getTo().getSimpleName());
+			rE.setAttribute("from", r.getFrom().getFQName());
+			rE.setAttribute("to", r.getTo().getFQName());
 			relationshipsE.appendChild(rE);
 		}
-		classDiagrams.appendChild(relationshipsE);
+		return relationshipsE;
+//		classDiagrams.appendChild(relationshipsE);
 	}
+//	private static void parseRelationships(Set<Relationship> relationships) {
+//		Element relationshipsE = doc.createElement("relationships");
+//		for (Relationship r : relationships) {
+//			Element rE = doc.createElement("relationship");
+//			rE.setAttribute("type", r.getType());
+//			rE.setAttribute("from", r.getFrom().getSimpleName());
+//			rE.setAttribute("to", r.getTo().getSimpleName());
+//			relationshipsE.appendChild(rE);
+//		}
+//		classDiagrams.appendChild(relationshipsE);
+//	}
 
 	private static void writeXml(Document doc, String outputFile) {
 
